bb84fc0e571539ab042331bfaf202f4ad23bf6f5
Joaoaalves <joaoaa21@hotmail.com>
2024-07-19 09:52:19 -0300

    Cookie compliance context, hook and component created

---
+++ contexts/CookieConsentContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { getCookie, setCookie } from "cookies-next";
+
+const CookieConsentContext = createContext()
+
+export const CookieConsentProvider = ({children}) => {
+  const [showConsent, setShowConsent] = useState(false);
+
+  useEffect(() => {
+    const consent = getCookie('cookieConsent');
+
+    if (!consent){
+      setShowConsent(true);
+    }
+  }, []);
+
+  const acceptCookie = (value) => {
+    
+    if(!['all', 'necessary'].includes(value))
+      throw new Error('Cookie consent value must be "all" or "necessary"')
+    
+    setCookie('cookieConsent', value, {maxAge: 60 * 60 * 24 * 365});
+
+    setShowConsent(false);
+  }
+
+  return (
+    <CookieConsentContext.Provider value={{ showConsent, setShowConsent, acceptCookie}}>
+      {children}
+    </CookieConsentContext.Provider>
+  )
+}
+
+export const useCookieConsent = () => {
+  const context = useContext(CookieConsentContext);
+  if (!context) {
+    throw new Error('useCookieConsent must be used within a CookieConsentProvider');
+  }
+  return context;
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
-
+import { CookieConsentProvider } from "@/contexts/CookieConsentContext";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <CookieConsentProvider>
+    <Component {...pageProps} />
+  </CookieConsentProvider>;
 }
